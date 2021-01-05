52d4e827205470f0fbb6b508864c3c704f9d9069
Jerico Suguitan <jsuguitan.cse@gmail.com>
2021-01-04 21:05:18 -0800

    update

--- settings.py
+++ settings.py
@@ -1,33 +1,33 @@
-import os
-from os.path import join, dirname
-from dotenv import load_dotenv
-
 # import os
-# from dotenv import load_dotenv, find_dotenv
-
-# load_dotenv(find_dotenv())
-
-dotenv_path = join(dirname(__file__), '.env')
-load_dotenv(dotenv_path)
-
-BOT_TOKEN = os.environ.get("BOT_TOKEN")
-PREFIX = os.environ.get("PREFIX")
-
-# # settings.py
+# from os.path import join, dirname
 # from dotenv import load_dotenv
-# load_dotenv()
 
-# # OR, the same with increased verbosity
-# load_dotenv(verbose=True)
+# # import os
+# # from dotenv import load_dotenv, find_dotenv
 
-# # OR, explicitly providing path to '.env'
-# from pathlib import Path  # Python 3.6+ only
-# env_path = Path('.') / '.env'
-# load_dotenv(dotenv_path=env_path)
+# # load_dotenv(find_dotenv())
 
-# import os
-# BOT_TOKEN = os.getenv("BOT_TOKEN")
-# PREFIX = os.getenv("PREFIX")
-# # SECRET_KEY = os.getenv("EMAIL")
-# # DATABASE_PASSWORD = os.getenv("DATABASE_PASSWORD")
+# dotenv_path = join(dirname(__file__), '.env')
+# load_dotenv(dotenv_path)
+
+# BOT_TOKEN = os.environ.get("BOT_TOKEN")
+# PREFIX = os.environ.get("PREFIX")
+
+# # # settings.py
+# # from dotenv import load_dotenv
+# # load_dotenv()
+
+# # # OR, the same with increased verbosity
+# # load_dotenv(verbose=True)
+
+# # # OR, explicitly providing path to '.env'
+# # from pathlib import Path  # Python 3.6+ only
+# # env_path = Path('.') / '.env'
+# # load_dotenv(dotenv_path=env_path)
+
+# # import os
+# # BOT_TOKEN = os.getenv("BOT_TOKEN")
+# # PREFIX = os.getenv("PREFIX")
+# # # SECRET_KEY = os.getenv("EMAIL")
+# # # DATABASE_PASSWORD = os.getenv("DATABASE_PASSWORD")
 
